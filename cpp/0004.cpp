eb5a612094ae6c3c623ea4363bcd0ba453bec516
nutthanon <nutthanon.tho@gmail.com>
2021-07-07 14:37:33 +0700

    Beta

--- cpp/0004.cpp
+++ cpp/0004.cpp
@@ -1,1 +1,29 @@
-4
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    int a[m][n], b[m][n];
+
+    for(int i=0;i<m;i++) {
+        for(int j=0;j<n;j++) {
+            cin >> a[i][j];
+        }
+    }
+    for(int i=0;i<m;i++) {
+        for(int j=0;j<n;j++) {
+            cin >> b[i][j];
+        }
+    }
+    for(int i=0;i<m;i++) {
+        for(int j=0;j<n;j++) {
+            cout << a[i][j] + b[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+
+    return 0;
+}
