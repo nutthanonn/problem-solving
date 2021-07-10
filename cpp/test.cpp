10e54248348205738db21d09a1b65215da6a327a
nutthanon <nutthanon.tho@gmail.com>
2021-07-10 20:01:57 +0700

    beta

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a, b,c;
-    cin >> a >> b;
-    c = a;
-for(int i=1;i<b;i++){
-        a *= c;
+int main(){
+    string a;
+    getline(cin, a);
+    for(int i=0;i<a.length();i++){
+        if(a[i] == 'A' || a[i] == 'a' ||  a[i] == 'E' || a[i] == 'e' || a[i] == 'I' || a[i] == 'i' || a[i] == 'O' || a[i] == 'o' || a[i] == 'U' || a[i] == 'u'){
+            a[i] = '\n';
+        }
     }
     cout << a;
     return 0;
