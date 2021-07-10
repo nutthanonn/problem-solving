2aea8c494f4a9e8f6834e4425b4b66fcd3b7c9da
nutthanon <nutthanon.tho@gmail.com>
2021-07-10 14:38:55 +0700

    beta

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -2,28 +2,12 @@
 using namespace std;
 
 int main() {
-    int a, b;
-    cin >> a;
-    if(a == 1){
-        cout << "#";
-    } else if(a == 2){
-        cout << "##" << endl << "##";
-    }else{
-        for(int i=0;i<a;i++){
-            cout << "#";
-        }
-        cout << endl;
-        b = a - 2;
-        for(int j=0;j<b;j++){
-            cout << "#";
-            for(int k=0;k<b;k++){
-                cout << " ";
-            }
-            cout << "#" << endl;
-        }
-        for(int i=0;i<a;i++){
-            cout << "#";
-        }
+    int a, b,c;
+    cin >> a >> b;
+    c = a;
+for(int i=1;i<b;i++){
+        a *= c;
     }
+    cout << a;
     return 0;
 }
