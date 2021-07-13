a6eeb59fcf256e2de05d365184a6f3e45028d6b8
nutthanon <nutthanon.tho@gmail.com>
2021-07-13 18:24:42 +0700

    beta

--- python_learn/class.py
+++ python_learn/class.py
@@ -1,23 +1,32 @@
+#สร้าง Class ง่ายๆ
 class school:
-    def __init__(self, teacher, student, name):
-        self.teacher = teacher
-        self.student = student
-        self.name = name
+    def __init__(self, name, userID):
+        self._name = name
+        self.__userID = userID
 
-    def show(self):
-        print("Name : " + self.name)
-
-
-
+    def _showData(self):
+        print("Name : {}".format(self._name))
+        print("USER-ID : {}".format(self.__userID))
 
 class Teacher(school):
-    def __init__(self, department, salary, name):
-        self.department = department
-        self.salary = salary
-        self.name = name
-        super().__init__(self.name)
-        super().show()
+    def __init__(self, name, userID, subject):
+        super().__init__(name, userID)
+        self.subject = subject
+        
+    def _showData(self):
+        super()._showData()
+        print("Subject : {}".format(self.subject))
 
 
-obj1 = Teacher("math", 40000, "NUTTHANON")
+class student(school):
+    def __init__(self, name, userID, section):
+        super().__init__(name, userID)
+        self.section = section
 
+    def _showData(self):
+        super()._showData()
+        print("Section : {}".format(self.section))
+
+
+obj1 = Teacher("nut", 1150, "math")
+obj1._showData()
