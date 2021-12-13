4e4df9a38f31a9ccf03a368932c1deb4026a5225
nutthanon <nutthanon.tho@gmail.com>
2021-12-13 14:51:35 +0700

    :tada: borntodev-official-task

--- borntodev/1-star/ร้านของลุงสมหมาย.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    cout << "Welcome to Sommai 108 Eleven Shop";
-    return 0;
-}
--- borntodev/1-star/ร้านลุงสมหมาย2.0.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main (){
-    string a;
-    getline(cin, a);
-    cout << "Welcome " << a << " !" << endl << "Sommai 108 Eleven Shop";
-    return 0;
-}
