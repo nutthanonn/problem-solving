2ae35b70e6bb57e318c7ad1ef9941762363c3952
nutthanon <nutthanon.tho@gmail.com>
2022-04-06 13:05:46 +0700

    [+] 0278_FirstBadVersion.go

---
+++ Leetcode/Go/0278_FirstBadVersion.go
@@ -0,0 +1,18 @@
+package main
+
+func firstBadVersion(n int) int {
+	first := 1
+	last := n
+	for first <= last {
+		mid := int((first + last) / 2)
+		if !isBadVersion(mid) {
+			first = mid + 1
+		} else {
+			last = mid - 1
+		}
+	}
+	return first
+}
+func main() {
+
+}
