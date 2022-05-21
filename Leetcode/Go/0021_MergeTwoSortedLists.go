a0e9066aa8350f52830f2396b17e125bd3f5fc24
nutthanon <nutthanon.tho@gmail.com>
2022-05-21 14:47:25 +0700

    [+] 0021_MergeTwoSortedLists.go

--- Leetcode/Go/0021_MergeTwoSortedLists.go
+++ Leetcode/Go/0021_MergeTwoSortedLists.go
@@ -1,111 +1,28 @@
-// เเนวคิด
-
 package main
 
-import "fmt"
-
-type Node struct {
+type ListNode struct {
 	Val  int
-	next *Node
+	Next *ListNode
 }
 
-type linkedlist struct {
-	head *Node
-	size int
-}
-
-func merge(a []int, b []int) []int {
-	final := []int{}
-	i := 0
-	j := 0
-	for i < len(a) && j < len(b) {
-		if a[i] < b[j] {
-			final = append(final, a[i])
-			i++
-		} else {
-			final = append(final, b[j])
-			j++
-		}
-	}
-	for ; i < len(a); i++ {
-		final = append(final, a[i])
-	}
-	for ; j < len(b); j++ {
-		final = append(final, b[j])
-	}
-	return final
-}
-
-func mergeSort(items []int) []int {
-	if len(items) < 2 {
-		return items
-	}
-	first := mergeSort(items[:len(items)/2])
-	second := mergeSort(items[len(items)/2:])
-	return merge(first, second)
-}
-
-func (l *linkedlist) Insert(val int) {
-
-	new_node := Node{}
-	new_node.Val = val
-	if l.size == 0 {
-		l.head = &new_node
-		l.size++
-		return
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	if list1 == nil {
+		return list2
 	}
 
-	ptr := l.head
-	for i := 0; i < l.size; i++ {
-		if ptr.next == nil {
-			ptr.next = &new_node
-			l.size++
-			return
-		}
-		ptr = ptr.next
+	if list2 == nil {
+		return list1
 	}
-}
 
-func mergeTwoLists(l1, l2 *linkedlist) {
-	arr := []int{}
-	ptr1 := l1.head
-	ptr2 := l2.head
-	for i := 0; i < l1.size; i++ {
-		arr = append(arr, ptr1.Val)
-		ptr1 = ptr1.next
-	}
-	for i := 0; i < l2.size; i++ {
-		arr = append(arr, ptr2.Val)
-		ptr2 = ptr2.next
-	}
-	arr = mergeSort(arr)
-	l1.head = nil
-	l1.size = 0
-	for i := 0; i < len(arr); i++ {
-		l1.Insert(arr[i])
-	}
-}
-func (l *linkedlist) DataList() {
-	ptr := l.head
-	for i := 0; i < l.size; i++ {
-		fmt.Println(ptr.Val)
-		ptr = ptr.next
+	if list1.Val <= list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoLists(list1, list2.Next)
+		return list2
 	}
 }
 
 func main() {
-	l1 := linkedlist{}
-	l2 := linkedlist{}
-
-	l1.Insert(1)
-	l1.Insert(2)
-	l1.Insert(4)
-
-	l2.Insert(1)
-	l2.Insert(3)
-	l2.Insert(4)
-
-	mergeTwoLists(&l1, &l2)
-	l1.DataList()
 
 }
