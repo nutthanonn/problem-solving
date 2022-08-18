6f6f5672bd10e48e5100f85aeeceb387a50de928
nutthanon <nutthanon.tho@gmail.com>
2022-08-18 16:21:21 +0700

    [+] add :sql sol

---
+++ Leetcode/Sql/0586_Customer-Placing-the-Largest-Number-of-Orders.sql
@@ -0,0 +1,5 @@
+select customer_number
+from Orders
+group by customer_number
+order by count(order_number) desc
+limit 1;
---
+++ Leetcode/Sql/0620_Not-Boring-Movies.sql
@@ -0,0 +1,4 @@
+select id, movie, description, rating
+from Cinema
+where mod(id,2)=1 and description <> 'boring'
+order by rating desc;
